093ed728e8901d5ce3acb9f139909d6675b9e396
Carlos Perez <37264926+CPerezz@users.noreply.github.com>
2018-12-08 18:29:05 +0100

    Correct Empty Socket connection error.
    Added Ok(0) that appears when you try to recieve from a Socket that hasn't a connection.

--- chat/server/src/main.rs
+++ chat/server/src/main.rs
@@ -1,59 +1,90 @@
+extern crate dotenv;
+
 use std::io::{ErrorKind, Read, Write};
+use std::time::Duration;
 use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
+use dotenv::dotenv;
+use std::env;
+mod utils;
 
-const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 32;
 
-fn sleep() {
-    thread::sleep(::std::time::Duration::from_millis(100));
-}
+fn main () {
+    //Charge envoirment variables.
+    dotenv().ok().expect(".env file not present");
+    let host: String = env::var("HOST").expect("Error reading .env variable.");
+    
+    //Instanciate the server
+    let server = TcpListener::bind(&host).expect("Listener failet to bind.");
 
-fn main() {
-    let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
-    server.set_nonblocking(true).expect("failed to initialize non-blocking");
+    //Set server in Non-blocking state to force it to hear for changes all the time.
+    server.set_nonblocking(true).expect("Failed to set Non-Blocking state.");
 
-    let mut clients = vec![];
-    let (tx, rx) = mpsc::channel::<String>();
-    loop {
+    //Generating a clients Storage Vector.
+    let mut clients: Vec<std::net::TcpStream> = vec![];
+
+    //An asynchronous, infinitely buffered channel. The channel function will return a (Sender, Receiver) tuple where all sends will be asynchronous.
+    //Note that we've specified that Strings will be the types that travel through the channel.
+    let (sender, reciever) = mpsc::channel::<String>();
+    loop{
+        //If the recieved connection from the listener contains a correct value, is accepted.
         if let Ok((mut socket, addr)) = server.accept() {
-            println!("Client {} connected", addr);
+            println!("Client {:?} connected to the chhanel!", addr);
 
-            let tx = tx.clone();
-            clients.push(socket.try_clone().expect("failed to clone client"));
+            let sender = sender.clone();
+            //Clone the socket to push it into a thread.
+            clients.push(socket.try_clone().expect("Failed to clone the client."));
 
             thread::spawn(move || loop {
+                //Create a buffer to store the msges.
                 let mut buff = vec![0; MSG_SIZE];
 
-                match socket.read_exact(&mut buff) {
-                    Ok(_) => {
-                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                //Hear socket entries from sender an match it with a Result.
+                match socket.read(&mut buff) {
 
-                        println!("{}: {:?}", addr, msg);
-                        tx.send(msg).expect("failed to send msg to rx");
-                    }, 
-                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
-                    Err(_) => {
-                        println!("closing connection with: {}", addr);
+                    //a read() syscall on a socket that has been closed on the other end will return 0 bytes read, 
+                    //but no error, which should translate to Ok(0) in Rust. 
+                    //But this may only apply when the other end closed the connection cleanly.
+                    Ok(0) => {
+                        println!("\nClient: {} left the channel.", addr);
                         break;
                     }
+
+                    //Handle when we do not read an empty socket
+                    Ok(_) => {
+                        //Set the buffer as an Iretartor and take it's elements while the condition retunrs true. Finally returns a Vec of type T
+                        let msg = buff.clone().into_iter().take_while(|&x| x!= 0).collect::<Vec<_>>();
+
+                        println!("\nMSG as Bytes:   {:?}", msg.clone());
+                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+
+                        println!("\n{}: {:?}", addr, msg);
+                        sender.send(msg).expect("failed to send msg to reciever");
+                    },
+                    //Handle reading errors!
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(_) => {
+                        println!("\nClient: {} left the channel.", addr);
+                        break;
+                    }
+                    
                 }
 
-                sleep();
+                thread::sleep(Duration::from_millis(200));
             });
         }
-
-        if let Ok(msg) = rx.try_recv() {
+        //A bit of functionall programming to send the message to all the other chat members.
+        if let Ok(msg) = reciever.try_recv() {
             clients = clients.into_iter().filter_map(|mut client| {
-                let mut buff = msg.clone().into_bytes();
-                buff.resize(MSG_SIZE, 0);
+                let buff = msg.clone().into_bytes();
+                buff.clone().resize(buff.len(), 0);
 
                 client.write_all(&buff).map(|_| client).ok()
             }).collect::<Vec<_>>();
         }
 
-        sleep();
+        thread::sleep(Duration::from_millis(200));
     }
 }
